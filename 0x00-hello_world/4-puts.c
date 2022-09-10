5033d2feeb6094cbeaf0b0cb20f669d59e821569
Cynthia Aniehe <ogecynthia1@yahoo.com>
2022-09-09 18:47:41 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**code on using
+ * put function
+ */
+int main()
+{
+	/*string initialization*/
+	char Mystr[] = "Programming is like building a multilingual puzzle\n";
+	puts(Mystr);
+	return 0;
+}
+
