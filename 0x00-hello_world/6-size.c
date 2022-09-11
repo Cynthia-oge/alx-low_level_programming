5bdf058a20a999310a5e47bff69bbfc27049551a
Cynthia Aniehe <ogecynthia1@yahoo.com>
2022-09-11 14:23:35 -0700

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,6 @@
 
 int main(void)
 {
-
-	//evaluate the size of types
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long));
