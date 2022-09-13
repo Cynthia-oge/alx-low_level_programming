c7fb91b494cf3039bcb10b906e854b817d48e89e
Cynthia Aniehe <ogecynthia1@yahoo.com>
2022-09-12 20:29:14 -0700

    0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - 'the program's description'
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int C;
+
+	for (C = 0; C < 10; ++C)
+		printf("%d", C);
+	     printf("\n");
+	return (0);
+}
