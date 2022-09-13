dab7e6a340ea253189595d678126a1ad239f3b3c
Cynthia Aniehe <ogecynthia1@yahoo.com>
2022-09-12 20:37:47 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - 'the program's description'
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int C;
+
+	for (C = '0'; C <= '9'; ++C)
+		putchar(C);
+	     putchar('\n');
+	return (0);
+}
