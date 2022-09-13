134353535a0018227f9f6d72c07da797dde15d2a
Cynthia Aniehe <ogecynthia1@yahoo.com>
2022-09-12 19:57:35 -0700

    a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - 'the program's description'
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char C;
+
+	for (C = 'a'; C <= 'z'; ++C)
+		putchar(C);
+	return (0);
+}
