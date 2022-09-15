a633837e3c98eed92720bca510fd06282a4a7370
Cynthia Aniehe <ogecynthia1@yahoo.com>
2022-09-14 20:26:41 -0700

    alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> print lower cases 10x;
+ */
+
+void print_alphabet_x10(void)
+{
+	int 1ox;
+	int i;
+
+	for (10x = 1; 10x <= 10; 10x++)
+{
+	for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
+	_putchar('\n');
+}
