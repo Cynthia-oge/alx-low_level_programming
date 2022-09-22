4cae0fcd6dd48bac050e0fc900d09a45fec1daeb
Aniehe Cynthia <ogecynthia1@yahoo.com>
2022-09-22 04:50:24 +0100

    string concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,14 +4,18 @@
  * *_strcat() => a function to concatenate two string
  * @dest: a variable to concatenate
  * @src: the second variable
- * Return: always 0 (success)
+ * Return: returns pointer to @dest 
  */
 
 char *_strcat(char *dest, char *src)
 {
-	dest[] = "Hello ";
-	src[] = "World!\n";
-	dest = strcat( *dest, *src);
-	_putchar("%s", dest);
-	return (0);
+	int i = 0, j = 0;
+
+	while (dest[i++])
+		j++;
+
+	for (i = 0; src[i]; i++)
+		dest[j++] = src[i];
+
+	return (dest);
 }
